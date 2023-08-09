bb8c634e1f4e394999e6ae2525300787cdde3ec1
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-08-10 01:43:56 +0530

    added headers

--- app/src/main/java/com/rizwansayyed/zene/ui/home/homeui/HomepageView.kt
+++ app/src/main/java/com/rizwansayyed/zene/ui/home/homeui/HomepageView.kt
@@ -2,9 +2,17 @@ package com.rizwansayyed.zene.ui.home.homeui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.rizwansayyed.zene.BaseApplication.Companion.dataStoreManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 @Composable
 fun HomepageView() {
+    val header by dataStoreManager.albumHeaderData.collectAsState(initial = runBlocking(Dispatchers.IO) { dataStoreManager.albumHeaderData.first() })
+
     Column {
 
     }
