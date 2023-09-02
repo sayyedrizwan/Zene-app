29866fea07692d0fa150e19cdc8bb860a11e0ffa
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-09-02 15:34:39 +0530

    completed remember scroll state..

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.rizwansayyed.zene"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.000333"
+        versionCode = 8
+        versionName = "1.0.000334"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
