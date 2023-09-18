c7e1d138ecc03d666ec9bfeec8cccb9e357642cf
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-09-18 22:09:29 +0530

    added packages

--- app/src/main/java/com/rizwansayyed/zene/presenter/MainActivity.kt
+++ app/src/main/java/com/rizwansayyed/zene/presenter/MainActivity.kt
@@ -1,19 +1,8 @@
 package com.rizwansayyed.zene.presenter
 
-import android.os.Build
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rizwansayyed.zene.presenter.theme.ZeneTheme
 import com.rizwansayyed.zene.presenter.ui.MainHomePageView
 import com.rizwansayyed.zene.presenter.util.UiUtils.transparentStatusAndNavigation
