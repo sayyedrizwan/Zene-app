c104bb16fc6ccc648ba754c90244446ad039c82f
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-11-12 12:37:52 +0530

    working on artists profileview

--- app/src/main/java/com/rizwansayyed/zene/presenter/ui/home/artists/ArtistsSocialAndProfileView.kt
+++ app/src/main/java/com/rizwansayyed/zene/presenter/ui/home/artists/ArtistsSocialAndProfileView.kt
@@ -1,13 +1,18 @@
 package com.rizwansayyed.zene.presenter.ui.home.artists
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -26,6 +34,8 @@ import com.rizwansayyed.zene.presenter.theme.MainColor
 import com.rizwansayyed.zene.presenter.ui.SmallIcons
 import com.rizwansayyed.zene.presenter.ui.TextBold
 import com.rizwansayyed.zene.presenter.ui.TextRegular
+import com.rizwansayyed.zene.presenter.ui.TextThin
+import com.rizwansayyed.zene.presenter.ui.shimmerBrush
 import com.rizwansayyed.zene.utils.Utils.formatNumberToFollowers
 import com.rizwansayyed.zene.viewmodel.ArtistsViewModel
 
@@ -74,14 +84,55 @@ private val socialMedia = listOf(
 )
 
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ArtistsSocialMedia() {
     ArtistsSocialMediaLoading()
+
+    FlowRow {
+        repeat(10) {
+            ArtistsSocialMediaButton(R.drawable.ic_internet, stringResource(R.string.website))
+        }
+    }
 }
 
 @Composable
 fun ArtistsSocialMediaLoading() {
+    Row(Modifier.fillMaxWidth(), Arrangement.Center, Alignment.CenterVertically) {
+        Spacer(
+            Modifier
+                .padding(5.dp)
+                .weight(1f)
+                .clip(RoundedCornerShape(12.dp))
+                .background(shimmerBrush())
+        )
 
+        Spacer(
+            Modifier
+                .padding(5.dp)
+                .weight(1f)
+                .clip(RoundedCornerShape(12.dp))
+                .background(shimmerBrush())
+        )
+    }
+}
+
+@Composable
+fun ArtistsSocialMediaButton(img: Int, s: String) {
+    Column(Modifier.width(LocalConfiguration.current.screenWidthDp.dp / 3)) {
+        Image(
+            painterResource(img),
+            "",
+            Modifier
+                .padding(5.dp)
+                .size(40.dp)
+                .clip(RoundedCornerShape(50))
+                .background(Color.Black)
+                .padding(5.dp)
+        )
+
+        TextThin(s, Modifier.fillMaxWidth(), doCenter = true)
+    }
 }
 
 
