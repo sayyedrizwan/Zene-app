862897a790abb9856d58ff9d419a7ff4a2596de2
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-11-14 20:34:12 +0530

    working on artists and booking events..

--- app/src/main/java/com/rizwansayyed/zene/presenter/ui/home/artists/ArtistsEventsView.kt
+++ app/src/main/java/com/rizwansayyed/zene/presenter/ui/home/artists/ArtistsEventsView.kt
@@ -1,33 +1,30 @@
 package com.rizwansayyed.zene.presenter.ui.home.artists
 
-import android.net.Uri
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -35,12 +32,9 @@ import com.rizwansayyed.zene.R
 import com.rizwansayyed.zene.data.DataResponse
 import com.rizwansayyed.zene.domain.ArtistsEvents
 import com.rizwansayyed.zene.presenter.theme.MainColor
-import com.rizwansayyed.zene.presenter.ui.TextBold
-import com.rizwansayyed.zene.presenter.ui.TextRegular
 import com.rizwansayyed.zene.presenter.ui.TextSemiBold
 import com.rizwansayyed.zene.presenter.ui.TextThin
 import com.rizwansayyed.zene.presenter.ui.TopInfoWithSeeMore
-import com.rizwansayyed.zene.utils.Utils.customBrowser
 import com.rizwansayyed.zene.viewmodel.ArtistsViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -57,7 +51,7 @@ fun ArtistsEvents() {
         is DataResponse.Success -> {
             val pager = rememberPagerState(pageCount = { v.item?.size ?: 0 })
             HorizontalPager(pager, Modifier.fillMaxWidth()) { page ->
-                EventsItems(v.item?.get(page)!!)
+                EventsItems(v.item?.get(page)!!, artistsViewModel)
             }
 
             Spacer(Modifier.height(15.dp))
@@ -82,88 +76,145 @@ fun ArtistsEvents() {
 }
 
 @Composable
-fun EventsItems(events: ArtistsEvents) {
-    Row(
+fun EventsItems(events: ArtistsEvents, artist: ArtistsViewModel) {
+    Box(
         Modifier
             .padding(5.dp)
             .fillMaxWidth()
-            .height(intrinsicSize = IntrinsicSize.Max)
-            .wrapContentHeight()
-            .clip(RoundedCornerShape(14.dp))
-            .background(Color.Black),
-        Arrangement.Center, Alignment.CenterVertically
+            .height(LocalConfiguration.current.screenWidthDp.dp)
+            .clip(RoundedCornerShape(6))
+            .background(Color.Black)
     ) {
-        Column(Modifier.weight(1f)) {
-            Spacer(Modifier.height(23.dp))
+        val img: String = if (events.artists.isNotEmpty()) events.artists[0].img else
+            when (val v = artist.artistsImage) {
+                is DataResponse.Success -> v.item
+                else -> ""
+            }
+        AsyncImage(img, events.eventName, Modifier.fillMaxSize(), contentScale = ContentScale.Crop)
 
-            TextBold(
-                events.eventName ?: "",
-                Modifier
-                    .padding(start = 10.dp)
-                    .fillMaxWidth(), size = 25
-            )
+        Spacer(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(0.4f))
+        )
 
-            val size = events.address?.split(",")
+        Row(
+            Modifier
+                .align(Alignment.TopCenter)
+                .padding()
+                .padding(top = 10.dp, end = 10.dp)
+        ) {
 
-            if ((size?.size ?: 0) > 3)
-                TextThin(
-                    "${size?.get(0)}, ${size?.get(size.size - 1)}",
-                    Modifier
-                        .padding(start = 10.dp, top = 9.dp)
-                        .fillMaxWidth(), size = 14
-                )
-            else
-                TextThin(
-                    events.address ?: "",
-                    Modifier
-                        .padding(start = 10.dp, top = 9.dp)
-                        .fillMaxWidth(), size = 14
-                )
-
-            Spacer(Modifier.weight(1f))
-
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(start = 12.dp, top = 19.dp)
-            ) {
-                events.artists.forEach {
+            Row {
+                events.artists.forEachIndexed { index, a ->
+                    if (index > 5) return@forEachIndexed
                     AsyncImage(
-                        it.img, it.name,
+                        a.img, a.name,
                         Modifier
-                            .padding(horizontal = 3.dp)
-                            .clip(RoundedCornerShape(50.dp))
-                            .size(40.dp), contentScale = ContentScale.Crop
+                            .size(40.dp)
+                            .clip(RoundedCornerShape(50))
+                            .border(1.dp, Color.White, RoundedCornerShape(50))
                     )
                 }
             }
 
-            Spacer(Modifier.height(23.dp))
-        }
-        Column(Modifier, Arrangement.Center, Alignment.CenterHorizontally) {
+            Spacer(Modifier.weight(1f))
+
             Column(
                 Modifier
-                    .padding(vertical = 20.dp, horizontal = 25.dp)
-                    .clip(RoundedCornerShape(14.dp))
+                    .clip(RoundedCornerShape(6))
                     .background(MainColor)
-                    .padding(7.dp),
-                Arrangement.Center,
-                Alignment.CenterHorizontally
+                    .padding(vertical = 5.dp, horizontal = 11.dp),
+                Arrangement.Center, Alignment.CenterHorizontally
             ) {
-                TextThin(events.date(), size = 17)
-                TextSemiBold(events.month(), size = 39)
-                TextThin(events.year(), size = 17)
+                TextThin(events.date(), size = 14)
+                TextSemiBold(events.month(), Modifier.offset(x = 4.dp, y = 0.dp), size = 34)
+                TextThin(events.year(), size = 14)
             }
-
-
-            Button(
-                onClick = { Uri.parse(events.link).customBrowser() },
-                colors = ButtonDefaults.buttonColors(MainColor)
-            ) {
-                TextThin(stringResource(R.string.book), color = Color.White)
-            }
-
-            Spacer(Modifier.height(13.dp))
         }
     }
+
+//    Row(
+//        Modifier
+//            .padding(5.dp)
+//            .fillMaxWidth()
+//            .height(intrinsicSize = IntrinsicSize.Max)
+//            .wrapContentHeight()
+//            .clip(RoundedCornerShape(14.dp))
+//            .background(Color.Black),
+//        Arrangement.Center, Alignment.CenterVertically
+//    ) {
+//        Column(Modifier.weight(1f)) {
+//            Spacer(Modifier.height(23.dp))
+//
+//            TextBold(
+//                events.eventName ?: "",
+//                Modifier
+//                    .padding(start = 10.dp)
+//                    .fillMaxWidth(), size = 25
+//            )
+//
+//            val size = events.address?.split(",")
+//
+//            if ((size?.size ?: 0) > 3)
+//                TextThin(
+//                    "${size?.get(0)}, ${size?.get(size.size - 1)}",
+//                    Modifier
+//                        .padding(start = 10.dp, top = 9.dp)
+//                        .fillMaxWidth(), size = 14
+//                )
+//            else
+//                TextThin(
+//                    events.address ?: "",
+//                    Modifier
+//                        .padding(start = 10.dp, top = 9.dp)
+//                        .fillMaxWidth(), size = 14
+//                )
+//
+//            Spacer(Modifier.weight(1f))
+//
+//            Row(
+//                Modifier
+//                    .fillMaxWidth()
+//                    .padding(start = 12.dp, top = 19.dp)
+//            ) {
+//                events.artists.forEach {
+//                    AsyncImage(
+//                        it.img, it.name,
+//                        Modifier
+//                            .padding(horizontal = 3.dp)
+//                            .clip(RoundedCornerShape(50.dp))
+//                            .size(40.dp), contentScale = ContentScale.Crop
+//                    )
+//                }
+//            }
+//
+//            Spacer(Modifier.height(23.dp))
+//        }
+//        Column(Modifier, Arrangement.Center, Alignment.CenterHorizontally) {
+//            Column(
+//                Modifier
+//                    .padding(vertical = 20.dp, horizontal = 25.dp)
+//                    .clip(RoundedCornerShape(14.dp))
+//                    .background(MainColor)
+//                    .padding(7.dp),
+//                Arrangement.Center,
+//                Alignment.CenterHorizontally
+//            ) {
+//                TextThin(events.date(), size = 17)
+//                TextSemiBold(events.month(), size = 39)
+//                TextThin(events.year(), size = 17)
+//            }
+//
+//
+//            Button(
+//                onClick = { Uri.parse(events.link).customBrowser() },
+//                colors = ButtonDefaults.buttonColors(MainColor)
+//            ) {
+//                TextThin(stringResource(R.string.book), color = Color.White)
+//            }
+//
+//            Spacer(Modifier.height(13.dp))
+//        }
+//    }
 }
