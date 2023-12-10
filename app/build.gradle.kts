17136fb27086d09bc8c344ee7bcd74427c8e055e
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-10 23:54:40 +0530

    working on version cleaning in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,24 @@
 import java.util.Properties
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
     kotlin("kapt")
-    id("com.google.devtools.ksp")
-    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = AndroidVersion.PACKAGE_NAME.version
-    compileSdk = CompileSDK.MAX_SDK.version
+    namespace = "com.rizwansayyed.zene"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = AndroidVersion.PACKAGE_NAME.version
-        minSdk = CompileSDK.MIN_SDK.version
-        targetSdk = CompileSDK.MAX_SDK.version
-        versionCode = CompileSDK.VERSION_CODE.version
-        versionName = AndroidVersion.APP_VERSION.version
+        applicationId = "com.rizwansayyed.zene"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 25
+        versionName = "1.0.000354"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -59,7 +59,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = AndroidVersion.KOTLIN_COMPILE.version
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -73,160 +73,107 @@ android {
 
 dependencies {
 
-//    implementation(libs.kotlin.core)
-//    implementation(libs.lifecycle.runtime)
-//    implementation(libs.activity.compose)
-//
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.ext.junit)
-//    androidTestImplementation(libs.expresso)
-//    androidTestImplementation(platform(libs.android.compose))
-//    androidTestImplementation(libs.ui.test)
-//    androidTestImplementation(libs.ui.tooling)
-//    androidTestImplementation(libs.ui.manifest)
-//
-//
-//    implementation(platform(libs.android.compose))
-//
-//    implementation(libs.compose.ui)
-//    implementation(libs.compose.ui.util)
-//    implementation(libs.compose.ui.graphics)
-//    implementation(libs.compose.ui.tooling.preview)
-//    implementation(libs.material3)
-//
-//
-//
-//
-//    implementation(libs.hilt)
-//    implementation(libs.hilt.compose)
-//    kapt(libs.hilt.compiler)
-//
-//
-//
-//    implementation(libs.coil)
-//    implementation(libs.datastore)
-//
-//
-//
-//    implementation(libs.coroutines.android)
-//    implementation(libs.coroutines.core)
-//
-//
-//
-//    implementation(libs.room)
-//    implementation(libs.room.compiler)
-//
-//
-//
-//    implementation(libs.retrofit)
-//    implementation(libs.retrofit.moshi)
-//    implementation(libs.retrofit.gson)
-//    implementation(libs.retrofit.scalars)
-//    implementation(libs.retrofit.simplexml)
-//    ksp(libs.moshi.codegen)
-//    implementation(libs.moshi)
-//
-//
-//    implementation(libs.browser)
-//    implementation(libs.js.evaluator)
-//
-//
-//
-//    implementation(libs.web)
-//    implementation(libs.jsoup)
-//
-//
-//
-//    implementation(platform(libs.okttp.bom))
-//    implementation(libs.okttp)
+    implementation(libs.kotlin.core)
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.activity.compose)
 
 
-    implementation("androidx.core:core-ktx:${DependenciesVersion.KOTLIN_VERSION.version}")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${DependenciesVersion.RUNTIME_KTX.version}")
-    implementation("androidx.activity:activity-compose:${DependenciesVersion.ACTIVITY_COMPOSE.version}")
-    implementation(platform("androidx.compose:compose-bom:${DependenciesVersion.COMPOSE.version}"))
-    implementation("androidx.compose.ui:ui-util:${DependenciesVersion.COMPOSE_UI_UTILS.version}")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:${TestingDependenciesVersion.JUnit.version}")
-    androidTestImplementation("androidx.test.espresso:espresso-core:${TestingDependenciesVersion.ESPRESSO.version}")
-    androidTestImplementation(platform("androidx.compose:compose-bom:${DependenciesVersion.COMPOSE.version}"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation("com.google.dagger:hilt-android:${DependenciesVersion.HILT.version}")
-    kapt("com.google.dagger:hilt-android-compiler:${DependenciesVersion.HILT.version}")
-    implementation("androidx.hilt:hilt-navigation-compose:${DependenciesVersion.HILT_COMPOSE.version}")
-
-    implementation("io.coil-kt:coil-compose:${DependenciesVersion.COIL.version}")
-
-    implementation("androidx.datastore:datastore-preferences:${DependenciesVersion.V.version}")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${DependenciesVersion.COROUTINES.version}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${DependenciesVersion.COROUTINES.version}")
 
 
-    implementation("androidx.room:room-ktx:${DependenciesVersion.ROOM.version}")
-    ksp("androidx.room:room-compiler:${DependenciesVersion.ROOM.version}")
-
-    implementation("com.squareup.retrofit2:retrofit:${DependenciesVersion.RETROFIT.version}")
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:${DependenciesVersion.MOSHI.version}")
-    implementation("com.squareup.moshi:moshi-kotlin:${DependenciesVersion.MOSHI.version}")
-    implementation("com.squareup.retrofit2:converter-moshi:${DependenciesVersion.RETROFIT.version}")
-    implementation("com.squareup.retrofit2:converter-gson:${DependenciesVersion.RETROFIT.version}")
-    implementation("com.squareup.retrofit2:converter-scalars:${DependenciesVersion.RETROFIT.version}")
-    implementation("com.squareup.retrofit2:converter-simplexml:${DependenciesVersion.RETROFIT.version}")
-
-    implementation("androidx.browser:browser:${DependenciesVersion.BROWSER.version}")
-    implementation("com.github.evgenyneu:js-evaluator-for-android:${DependenciesVersion.JS_EVALUATOR.version}")
-
-    implementation("org.jsoup:jsoup:${DependenciesVersion.JSOUP.version}")
-    implementation("androidx.webkit:webkit:${DependenciesVersion.WEBKIT.version}")
-
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:${DependenciesVersion.OK_HTTP.version}"))
-    implementation("com.squareup.okhttp3:okhttp")
-
-    implementation("androidx.work:work-runtime-ktx:${DependenciesVersion.WORK_MANAGER.version}")
-    implementation("androidx.hilt:hilt-work:${DependenciesVersion.HILT_COMPOSE.version}")
-    kapt("androidx.hilt:hilt-compiler:${DependenciesVersion.HILT_COMPOSE.version}")
-
-    implementation(platform("com.google.firebase:firebase-bom:${DependenciesVersion.FIREBASE.version}"))
-    implementation("com.google.firebase:firebase-config-ktx")
-    implementation("com.google.firebase:firebase-analytics-ktx")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.expresso)
+    androidTestImplementation(platform(libs.android.compose))
+    androidTestImplementation(libs.ui.test)
+    androidTestImplementation(libs.ui.tooling)
+    androidTestImplementation(libs.ui.manifest)
 
 
-    implementation("androidx.media3:media3-exoplayer:${DependenciesVersion.MEDIA3.version}")
-    implementation("androidx.media3:media3-ui:${DependenciesVersion.MEDIA3.version}")
-    implementation("androidx.media3:media3-session:${DependenciesVersion.MEDIA3.version}")
-    implementation("androidx.media3:media3-exoplayer-hls:${DependenciesVersion.MEDIA3.version}")
-    implementation("androidx.legacy:legacy-support-v4:${DependenciesVersion.V.version}")
 
-    implementation("androidx.paging:paging-runtime-ktx:${DependenciesVersion.PAGING.version}")
-    implementation("androidx.paging:paging-compose:${DependenciesVersion.PAGING_COMPOSE.version}")
+
+    implementation(platform(libs.android.compose))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.util)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.material3)
+
+
+
+
+    implementation(libs.hilt)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.work.manager)
+    implementation(libs.hilt.work.manager)
+
+
+
+
+    implementation(libs.coil)
+    implementation(libs.datastore)
+
+
+
+    implementation(libs.coroutines.android)
+    implementation(libs.coroutines.core)
+
+
+
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.gson)
+    implementation(libs.retrofit.scalars)
+    implementation(libs.retrofit.simplexml)
+    ksp(libs.moshi.codegen)
+    implementation(libs.moshi)
+
+
+
+    implementation(libs.browser)
+    implementation(libs.js.evaluator)
+
+
+
+    implementation(libs.web)
+    implementation(libs.jsoup)
+
+
+
+    implementation(platform(libs.okttp.bom))
+    implementation(libs.okttp)
+
+
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.config)
+
+
+
+    implementation(libs.paging.runtime)
+
+
+
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.ui)
+    implementation(libs.media3.session)
+    implementation(libs.media3.hls)
+    implementation(libs.media3.legacy)
 
 }
 
-enum class CompileSDK(val version: Int) {
-    MAX_SDK(34), MIN_SDK(24), VERSION_CODE(24)
-}
-
-enum class AndroidVersion(val version: String) {
-    PACKAGE_NAME("com.rizwansayyed.zene"), APP_VERSION("1.0.000354"), KOTLIN_COMPILE("1.5.3"),
-    ANDROID_VERSION("8.1.1")
-}
-
-enum class DependenciesVersion(val version: String) {
-    KOTLIN_VERSION("1.12.0"), RUNTIME_KTX("2.6.2"), ACTIVITY_COMPOSE("1.8.1"), COMPOSE("2023.09.00"),
-    HILT("2.48.1"), HILT_COMPOSE("1.1.0"), V("1.0.0"), COIL("2.4.0"), COROUTINES("1.7.3"), ROOM("2.6.1"),
-    RETROFIT("2.9.0"), MOSHI("1.14.0"), JSOUP("1.16.1"), OK_HTTP("4.10.0"), FIREBASE("32.3.1"),
-    MEDIA3("1.2.0"), COMPOSE_UI_UTILS("1.5.4"), JS_EVALUATOR("v6.0.0"), BROWSER("1.7.0"), WEBKIT("1.9.0"),
-    WORK_MANAGER("2.9.0"), PAGING("3.2.1"), PAGING_COMPOSE("3.3.0-alpha02")
-}
-
-enum class TestingDependenciesVersion(val version: String) {
-    JUnit("1.1.5"), ESPRESSO("3.5.1")
-}
+//enum class CompileSDK(val version: Int) {
+//    MAX_SDK(34), MIN_SDK(24), VERSION_CODE(24)
+//}
+//
+//enum class AndroidVersion(val version: String) {
+//    PACKAGE_NAME(), APP_VERSION("1.0.000354"), KOTLIN_COMPILE("1.5.3"),
+//    ANDROID_VERSION("8.1.1")
+//}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-buildscript {
-    val agp_version by extra("8.1.2")
-    val agp_version1 by extra("8.1.3")
-    val agp_version2 by extra("8.1.4")
-}
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.services) apply false
 }
