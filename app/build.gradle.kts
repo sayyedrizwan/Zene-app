3619ab3877289118ac9f10f4c238415d0f107cea
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2023-12-12 10:53:08 +0530

    pushed beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.rizwansayyed.zene"
         minSdk = 24
         targetSdk = 34
-        versionCode = 25
-        versionName = "1.0.000354"
+        versionCode = 26
+        versionName = "1.0.000356"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
