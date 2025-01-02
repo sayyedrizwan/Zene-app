2382f417367df957d1f310fded0a598a97a7d663
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-01-02 10:09:03 +0530

    working on login screen..

--- app/src/main/java/com/rizwansayyed/zene/ui/login/LoginView.kt
+++ app/src/main/java/com/rizwansayyed/zene/ui/login/LoginView.kt
@@ -1,9 +1,19 @@
 package com.rizwansayyed.zene.ui.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LoginView() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
 
+    }
 }
