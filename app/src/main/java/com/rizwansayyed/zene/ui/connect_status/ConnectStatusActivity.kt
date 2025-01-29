ab1e0e08e38fbe373171bed4580a4518090d6d7a
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-01-30 01:39:56 +0530

    connect profile deta.ils view..

---
+++ app/src/main/java/com/rizwansayyed/zene/ui/connect_status/ConnectStatusActivity.kt
@@ -0,0 +1,34 @@
+package com.rizwansayyed.zene.ui.connect_status
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.rizwansayyed.zene.ui.theme.DarkCharcoal
+import com.rizwansayyed.zene.ui.theme.ZeneTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ConnectStatusActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ZeneTheme {
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .background(DarkCharcoal)
+                ) {
+
+                }
+            }
+        }
+    }
+
+}
