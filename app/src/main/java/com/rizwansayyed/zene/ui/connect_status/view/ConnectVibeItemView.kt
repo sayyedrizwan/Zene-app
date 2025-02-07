f01422721e0c4a7d8c9b9b8db13cdffbf7420b0d
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-02-07 12:25:11 +0530

    updated the connect vibe captions

--- app/src/main/java/com/rizwansayyed/zene/ui/connect_status/view/ConnectVibeItemView.kt
+++ app/src/main/java/com/rizwansayyed/zene/ui/connect_status/view/ConnectVibeItemView.kt
@@ -3,8 +3,7 @@
 package com.rizwansayyed.zene.ui.connect_status.view
 
 import androidx.annotation.OptIn
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,11 +38,10 @@ import androidx.media3.ui.PlayerView
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.rizwansayyed.zene.R
 import com.rizwansayyed.zene.data.model.ConnectFeedDataResponse
-import com.rizwansayyed.zene.ui.view.ImageIcon
 import com.rizwansayyed.zene.ui.view.TextViewBold
 import com.rizwansayyed.zene.ui.view.TextViewNormal
+import com.rizwansayyed.zene.ui.view.TextViewSemiBold
 
 @Composable
 fun ConnectVibeItemView(item: ConnectFeedDataResponse?, showCaption: Boolean) {
@@ -54,80 +51,136 @@ fun ConnectVibeItemView(item: ConnectFeedDataResponse?, showCaption: Boolean) {
         Modifier
             .fillMaxWidth()
             .padding(horizontal = 3.dp)
+            .border(0.5.dp, Color.White, RoundedCornerShape(12.dp))
+            .padding(horizontal = 9.dp, vertical = 15.dp)
+            .padding(bottom = 10.dp)
     ) {
-        Row(Modifier.fillMaxWidth()) {
-            if (item?.media != null) {
-                Box(Modifier.weight(6f)) {
-                    ConnectVibeMediaItem(item, Modifier.align(Alignment.Center))
 
-                    if (item.isVibing == true) GlideImage(
-                        R.drawable.just_vibing_sticker,
-                        "",
-                        Modifier
-                            .align(Alignment.TopStart)
-                            .width(80.dp)
-                            .offset((-14).dp, (-28).dp)
-                            .rotate(-20f),
-                        contentScale = ContentScale.Fit
-                    )
-
-                    Row(
-                        Modifier
-                            .padding(9.dp)
-                            .align(Alignment.BottomEnd)
-                            .size(24.dp)
-                            .clickable { }
-                            .clip(RoundedCornerShape(100))
-                            .background(Color.White),
-                        Arrangement.Center,
-                        Alignment.CenterVertically) {
-                        ImageIcon(R.drawable.ic_arrow_expand, 20, Color.Black)
+        Row(Modifier.fillMaxWidth(), Arrangement.Center, Alignment.CenterVertically) {
+            GlideImage(
+                item?.userDetails?.profile_photo,
+                item?.userDetails?.name,
+                Modifier
+                    .size(45.dp)
+                    .clip(RoundedCornerShape(14.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(horizontal = 9.dp), Arrangement.Center, Alignment.Start
+            ) {
+                Spacer(Modifier.height(6.dp))
+                TextViewSemiBold(item?.userDetails?.name ?: "", 13, line = 1)
+                item?.userDetails?.username?.let {
+                    Box(Modifier.offset(y = (-5).dp)) {
+                        TextViewNormal("@${it}", 12, line = 1)
                     }
                 }
             }
-            if ((item?.jazz_name != null && item.jazz_id != null) || item?.emoji != null) Column(
-                Modifier
-                    .weight(4f)
-                    .padding(horizontal = 1.dp),
-                Arrangement.Center,
-                Alignment.CenterHorizontally
-            ) {
-                if (item.jazz_name != null && item.jazz_id != null) GlideImage(
-                    item.jazz_thumbnail,
-                    item.jazz_name,
-                    Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(1f),
-                    contentScale = ContentScale.Crop
-                )
-
-                Spacer(Modifier.padding(top = 12.dp))
-
-                if (item.emoji != null) TextViewBold(item.emoji ?: "", 50)
-            }
         }
 
-        if (item?.location_name != null) {
-            Row(
-                Modifier
-                    .padding(top = 25.dp, start = 3.dp)
-                    .fillMaxWidth(),
-                Arrangement.Center,
-                Alignment.CenterVertically
-            ) {
-                ImageIcon(R.drawable.ic_location, 18)
-                Spacer(Modifier.width(4.dp))
-                TextViewNormal(item.location_name!!, 14)
-                Spacer(Modifier.weight(1f))
-            }
+
+        if (item?.media == null && item?.jazz_id == null && item?.jazz_name == null && item?.caption != null) {
+            ConnectVibeItemOnlyCaption(item)
         }
 
-        if (showCaption && item?.caption != null) Box(Modifier.padding(top = 19.dp)) {
-            TextViewNormal(item.caption ?: "", 15)
-        }
+
+//        Row(Modifier.fillMaxWidth()) {
+//            if (item?.media != null) {
+//                Box(Modifier.weight(6f)) {
+//                    ConnectVibeMediaItem(item, Modifier.align(Alignment.Center))
+//
+//                    if (item.isVibing == true) GlideImage(
+//                        R.drawable.just_vibing_sticker,
+//                        "",
+//                        Modifier
+//                            .align(Alignment.TopStart)
+//                            .width(80.dp)
+//                            .offset((-14).dp, (-28).dp)
+//                            .rotate(-20f),
+//                        contentScale = ContentScale.Fit
+//                    )
+//
+//                    Row(
+//                        Modifier
+//                            .padding(9.dp)
+//                            .align(Alignment.BottomEnd)
+//                            .size(24.dp)
+//                            .clickable { }
+//                            .clip(RoundedCornerShape(100))
+//                            .background(Color.White),
+//                        Arrangement.Center,
+//                        Alignment.CenterVertically) {
+//                        ImageIcon(R.drawable.ic_arrow_expand, 20, Color.Black)
+//                    }
+//                }
+//            }
+//
+//            if ((item?.jazz_name != null && item.jazz_id != null) || item?.emoji != null) Column(
+//                Modifier
+//                    .weight(4f)
+//                    .padding(horizontal = 1.dp),
+//                Arrangement.Center,
+//                Alignment.CenterHorizontally
+//            ) {
+//                if (item.jazz_name != null && item.jazz_id != null) GlideImage(
+//                    item.jazz_thumbnail,
+//                    item.jazz_name,
+//                    Modifier
+//                        .fillMaxWidth()
+//                        .aspectRatio(1f),
+//                    contentScale = ContentScale.Crop
+//                )
+//
+//                Spacer(Modifier.padding(top = 12.dp))
+//
+//                if (item.emoji != null) TextViewBold(item.emoji ?: "", 50)
+//            }
+//        }
+//
+//        if (item?.location_name != null) {
+//            Row(
+//                Modifier
+//                    .padding(top = 25.dp, start = 3.dp)
+//                    .fillMaxWidth(),
+//                Arrangement.Center,
+//                Alignment.CenterVertically
+//            ) {
+//                ImageIcon(R.drawable.ic_location, 18)
+//                Spacer(Modifier.width(4.dp))
+//                TextViewNormal(item.location_name!!, 14)
+//                Spacer(Modifier.weight(1f))
+//            }
+//        }
+//
+//        if (showCaption && item?.caption != null) Box(Modifier.padding(top = 19.dp)) {
+//            TextViewNormal(item.caption ?: "", 15)
+//        }
     }
 
     Spacer(Modifier.height(10.dp))
+}
+
+
+@Composable
+fun ConnectVibeItemOnlyCaption(data: ConnectFeedDataResponse?) {
+    Spacer(Modifier.height(30.dp))
+    Box(Modifier.fillMaxWidth(), Alignment.Center) {
+        if (data?.caption?.trim() != null) {
+            TextViewBold(data.caption ?: "", 20, center = true)
+
+            if (data.emoji != null) Box(
+                Modifier
+                    .rotate(-20f)
+                    .offset(y = 30.dp, x = (-10).dp)
+                    .align(Alignment.BottomEnd)
+            ) {
+                TextViewBold(data.emoji ?: "", 50)
+            }
+        }
+    }
+    Spacer(Modifier.height(30.dp))
 }
 
 @OptIn(UnstableApi::class)
