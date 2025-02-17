a6d6e3a4cf5698ec71c55fa299fa0c6063d4d841
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-02-17 22:51:08 +0530

    working on zene music player....

--- app/src/main/java/com/rizwansayyed/zene/ui/musicplayer/MusicPlayerView.kt
+++ app/src/main/java/com/rizwansayyed/zene/ui/musicplayer/MusicPlayerView.kt
@@ -2,6 +2,7 @@ package com.rizwansayyed.zene.ui.musicplayer
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
@@ -9,22 +10,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -41,6 +52,7 @@ import com.rizwansayyed.zene.ui.theme.MainColor
 import com.rizwansayyed.zene.ui.view.ImageIcon
 import com.rizwansayyed.zene.ui.view.TextViewBold
 import com.rizwansayyed.zene.ui.view.TextViewNormal
+import com.rizwansayyed.zene.ui.view.TextViewSemiBold
 import com.rizwansayyed.zene.viewmodel.NavigationViewModel
 import kotlin.math.absoluteValue
 
@@ -55,7 +67,7 @@ fun MusicPlayerView(navViewModel: NavigationViewModel) {
         val configuration = LocalConfiguration.current
         val screenHeight = configuration.screenHeightDp.dp
         val screenWidth = (configuration.screenWidthDp / 1.1).dp
-        val topPadding = (screenHeight * 0.27f)
+        val topPadding = (screenHeight * 0.29f)
         val bottomPadding = (screenHeight * 0.16f)
         val pagerState = rememberPagerState(pageCount = { player?.lists?.size ?: 0 })
 
@@ -71,6 +83,13 @@ fun MusicPlayerView(navViewModel: NavigationViewModel) {
             ) { page ->
                 MusicPlayingView(player, page, pagerState, screenWidth)
             }
+
+            PlayerControlPanel(Modifier.align(Alignment.BottomCenter))
+        }
+
+        LaunchedEffect(player?.data?.id) {
+            val i = player?.lists?.indexOfFirst { it?.id == player?.data?.id }
+            pagerState.scrollToPage(i ?: 0)
         }
 
         BackHandler {
@@ -84,19 +103,23 @@ fun MusicPlayerView(navViewModel: NavigationViewModel) {
 fun MusicPlayingView(player: MusicPlayerData?, page: Int, pagerState: PagerState, screenWidth: Dp) {
     Column(
         Modifier
-            .fillMaxWidth()
+            .offset(
+                y = if (page == pagerState.currentPage) (-100).dp else 0.dp
+            )
+            .animateContentSize()
+            .fillMaxSize()
             .graphicsLayer {
                 val pageOffset =
                     (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
 
                 alpha = lerp(0.5f, 1f, 1f - pageOffset.absoluteValue.coerceIn(0f, 1f))
-            }, Arrangement.Center, Alignment.CenterHorizontally
+            }, Arrangement.Top, Alignment.CenterHorizontally
     ) {
         Box(Modifier.size(screenWidth, screenWidth)) {
             GlideImage(
                 player?.lists?.get(page)?.thumbnail,
                 player?.lists?.get(page)?.name,
-                Modifier.fillMaxSize(),
+                Modifier.fillMaxSize().clip(RoundedCornerShape(14.dp)),
                 contentScale = ContentScale.Fit
             )
 
@@ -110,8 +133,10 @@ fun MusicPlayingView(player: MusicPlayerData?, page: Int, pagerState: PagerState
             ) {
                 if (player?.data?.id == player?.lists?.get(page)?.id) {
                     GlideImage(
-                        R.raw.song_playing_wave, "",
-                        Modifier.size(25.dp), contentScale = ContentScale.Crop
+                        R.raw.song_playing_wave,
+                        "",
+                        Modifier.size(25.dp),
+                        contentScale = ContentScale.Crop
                     )
                 } else {
                     ImageIcon(R.drawable.ic_play, 20)
@@ -119,8 +144,85 @@ fun MusicPlayingView(player: MusicPlayerData?, page: Int, pagerState: PagerState
             }
         }
 
-        Spacer(Modifier.height(6.dp))
-        TextViewBold(player?.lists?.get(page)?.name ?: "", 25, center = true, line = 2)
-        TextViewNormal(player?.lists?.get(page)?.artists ?: "", 13, center = true, line = 1)
+        AnimatedVisibility(page == pagerState.currentPage) {
+            Column(Modifier.fillMaxWidth(), Arrangement.Center, Alignment.End) {
+                Spacer(Modifier.height(6.dp))
+                TextViewBold(player?.lists?.get(page)?.name ?: "", 25, center = true, line = 2)
+                TextViewNormal(player?.lists?.get(page)?.artists ?: "", 13, center = true, line = 1)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlayerControlPanel(modifier: Modifier = Modifier) {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+
+    Column(
+        modifier
+            .padding(horizontal = 15.dp, vertical = 10.dp)
+            .padding(bottom = 70.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(14.dp))
+            .background(Color.Black)
+            .padding(horizontal = 12.dp), Arrangement.Center, Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(40.dp))
+        Slider(value = sliderPosition,
+            onValueChange = {
+                sliderPosition = it
+            },
+            valueRange = 0f..100f,
+            colors = SliderDefaults.colors(Color.White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp),
+            track = { sliderState ->
+                SliderDefaults.Track(
+                    modifier = Modifier.height(4.dp),
+                    sliderState = sliderState,
+                    drawStopIndicator = null,
+                    thumbTrackGapSize = 0.dp,
+                    colors = SliderDefaults.colors(
+                        Color.White, MainColor, MainColor, Color.Gray, Color.Gray
+                    )
+                )
+            })
+
+        Spacer(Modifier.height(5.dp))
+
+        Row(Modifier.fillMaxWidth()) {
+            TextViewSemiBold("01:22", size = 13)
+            Spacer(Modifier.weight(1f))
+            TextViewSemiBold("01:11", size = 13)
+        }
+        Spacer(Modifier.height(9.dp))
+
+        Row(Modifier.fillMaxWidth(), Arrangement.SpaceEvenly, Alignment.CenterVertically) {
+            ImageIcon(R.drawable.ic_thumbs_up, 20)
+            Box(Modifier.rotate(180f)) {
+                ImageIcon(R.drawable.ic_forward, 27)
+            }
+            Box(
+                Modifier
+                    .padding(15.dp)
+                    .clip(RoundedCornerShape(100))
+                    .background(Color.White)
+                    .padding(10.dp)
+            ) {
+                ImageIcon(R.drawable.ic_play, 27, Color.Black)
+            }
+            Box(Modifier) {
+                ImageIcon(R.drawable.ic_forward, 27)
+            }
+
+            Box(Modifier) {
+                ImageIcon(R.drawable.ic_timer , 27)
+            }
+        }
+
+
+        Spacer(Modifier.height(40.dp))
     }
 }
