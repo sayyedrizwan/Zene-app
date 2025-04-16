cda9eee6719ad6789f37b52e35000345c6b15d74
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-04-17 02:22:53 +0530

    added banner ads

--- app/src/main/java/com/rizwansayyed/zene/utils/ads/BannerViewAds.kt
+++ app/src/main/java/com/rizwansayyed/zene/utils/ads/BannerViewAds.kt
@@ -1,38 +1,93 @@
 package com.rizwansayyed.zene.utils.ads
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.RequestConfiguration
+import kotlinx.coroutines.delay
+import java.util.Arrays
+import kotlin.math.roundToInt
+
 
 @Composable
 fun BannerViewAds() {
-    val context = LocalContext.current
+    val deviceCurrentWidth = LocalConfiguration.current.screenWidthDp
+    val padding = 16
+    var i by remember { mutableIntStateOf(0) }
+    var containerWidth by remember { mutableStateOf<Int?>(null) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
 
-    AndroidView(
         modifier = Modifier
-            .fillMaxWidth().height(120.dp),
-        factory = { ctx ->
-            AdView(ctx).apply {
-                val adSize = AdSize.getCurrentOrientationInlineAdaptiveBannerAdSize(context, 320)
+            .padding(padding.dp)
+            .fillMaxWidth()
+            .onSizeChanged {
+                containerWidth = it.width
+            }
+    ) {
+        val items =
+            listOf(
+                "deviceCurrentWidth - 40" to deviceCurrentWidth - 40,
+                "deviceCurrentWidth - padding * 2" to deviceCurrentWidth - padding * 2,
+                "AdSize.FULL_WIDTH" to AdSize.FULL_WIDTH,
+                "onSizeChanged" to with(LocalDensity.current) {
+                    containerWidth?.let { containerWidth ->
+                        (containerWidth / density).roundToInt()
+                    }
+                }
+            )
+        items.forEach {
+            val (title, width) = it
+            if (width == null) {
+                return@forEach
+            }
 
-                val bannerView = AdView(context)
-                bannerView.adUnitId = "ca-app-pub-3940256099942544/9214589741"
-                bannerView.setAdSize(adSize)
-
-// Step 3: Load an ad.
-                val adRequest = AdRequest.Builder().build()
-                bannerView.loadAd(adRequest)
+            Text(title)
+            AndroidView(
+                factory = { context ->
+                    AdView(context).apply {
+                        setAdSize( AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+                            context,
+                            width
+                        ))
+                        adUnitId = "ca-app-pub-2941808068005217/1624020934"
+                        loadAd(AdRequest.Builder().build())
+                    }
+                },
+                update = { adView ->
+                    adView.loadAd(AdRequest.Builder().build())
+                    i // needed to update view on i change
+                },
+            )
+        }
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1000)
+                i++
             }
         }
-    )
+    }
 }
