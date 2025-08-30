d730d0dde1a1bb113ac0335b21fbb040a13527dc
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2025-08-30 17:13:13 +0530

    Added safe checks for coroutines

--- app/src/main/java/com/rizwansayyed/zene/utils/SafeLaunch.kt
+++ app/src/main/java/com/rizwansayyed/zene/utils/SafeLaunch.kt
@@ -1,6 +1,5 @@
 package com.rizwansayyed.zene.utils
 
-import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -9,7 +8,6 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.cancellation.CancellationException
 
 fun CoroutineScope.safeLaunch(
-    tag: String = "",
     context: CoroutineContext = Dispatchers.IO,
     block: suspend CoroutineScope.() -> Unit
 ): Job {
@@ -17,11 +15,8 @@ fun CoroutineScope.safeLaunch(
         try {
             block()
         } catch (e: CancellationException) {
-            Log.d("safeLaunch", e.message ?: "")
             print(e)
         } catch (e: Exception) {
-            Log.d("safeLaunch", e.message ?: "")
-
             print(e)
         }
     }
